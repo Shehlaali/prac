07d9f835a0f6390f2081cf70988fad0ad53a5951
Shehlaali <94216278+Shehlaali@users.noreply.github.com>
2021-11-18 12:48:31 +0500

    Add files via upload

---
+++ 3.dart
@@ -0,0 +1,27 @@
+void main()
+
+{
+
+// QUESTION N0: 03 :
+
+ //var a = 2;
+ //var b = 1;
+ //var result= --a;       
+ //print(result);                                // Output: 1
+
+// var a = 2;
+// var b = 1;
+// var result= --a - --b;
+// print(result);                                // Output: 1
+
+//var a = 2;
+//var b = 1;
+//var result= --a - --b + ++b;
+//print(result);                                // Output: 2
+
+ var a = 2;
+ var b = 1;
+ var result= --a - --b + ++b + b--;
+ print(result);                                 // Output: 3
+
+}
