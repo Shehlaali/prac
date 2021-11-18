d5aa7a778131b68c3e4c37e24ba0a62960f7671c
Shehlaali <94216278+Shehlaali@users.noreply.github.com>
2021-11-18 12:49:06 +0500

    Add files via upload

---
+++ 5.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main()
+{
+
+// QUESTION N0: 05 :
+
+ stdout.write("Enter String To Show its Length :");
+ String shehla = stdin.readLineSync()!;
+ print(shehla.length);
+
+}
